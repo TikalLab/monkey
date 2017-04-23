1d2c2ab12d2b12ff3ccdb5fde5ed74908b235907
Shahar Solomianik <shahar@gnift.com>
2017-04-23 21:19:04 +0300

    content no null

--- app_modules/keys-finder.js
+++ app_modules/keys-finder.js
@@ -1,16 +1,21 @@
 module.exports = {
   find: function(content){
-    // var matches = content.match(/\b([a-zA-Z0-9]{12,})\b/g);
-    // var matches = content.match(/\b(?=[a-zA-Z0-9]{12,}).*\d+.*\b/g);
-    var matches = content.match(/\b(?=(?:[a-zA-Z]*\d){3})(?=.*\d[a-zA-Z]+\d)[a-zA-Z\d]{12,}\b/g);
+    var ret = null;
+
+    if(content){
+      // var matches = content.match(/\b([a-zA-Z0-9]{12,})\b/g);
+      // var matches = content.match(/\b(?=[a-zA-Z0-9]{12,}).*\d+.*\b/g);
+      var matches = content.match(/\b(?=(?:[a-zA-Z]*\d){3})(?=.*\d[a-zA-Z]+\d)[a-zA-Z\d]{12,}\b/g);
 
 
 
-    // var highRiskMatches = content.match(/(secret|token|key).+\b([a-zA-Z0-9]{16,})\b/g);
-    // var highRiskMatches = content.match(/(secret|token|key).+\b(?=[a-zA-Z0-9]{12,}).*\d+.*\b/g);
-    var highRiskMatches = content.match(/(secret|token|key).+\b(?=(?:[a-zA-Z]*\d){3})(?=.*\d[a-zA-Z]+\d)[a-zA-Z\d]{12,}\b/g);
+      // var highRiskMatches = content.match(/(secret|token|key).+\b([a-zA-Z0-9]{16,})\b/g);
+      // var highRiskMatches = content.match(/(secret|token|key).+\b(?=[a-zA-Z0-9]{12,}).*\d+.*\b/g);
+      var highRiskMatches = content.match(/(secret|token|key).+\b(?=(?:[a-zA-Z]*\d){3})(?=.*\d[a-zA-Z]+\d)[a-zA-Z\d]{12,}\b/g);
 
-    var ret = matches ? {matches: matches, severity: highRiskMatches ? 'high' : 'low'} : null;
+      ret = matches ? {matches: matches, severity: highRiskMatches ? 'high' : 'low'} : null;
+      
+    }
 
     return ret;
   }
